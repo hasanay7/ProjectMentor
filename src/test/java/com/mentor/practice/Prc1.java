8e55210e8592c2f1627358a4596e317ffb5f7835
Hasan Ay <hasanay777@gmail.com>
2021-11-14 12:00:51 +0300

    Comment added

--- src/test/java/com/mentor/practice/Prc1.java
+++ src/test/java/com/mentor/practice/Prc1.java
@@ -3,5 +3,7 @@ package com.mentor.practice;
 public class Prc1 {
     public static void main(String[] args) {
         System.out.println("what a wonderful world");
+        //new comment
+
     }
 }
