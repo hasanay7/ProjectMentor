3fdc9d090e8d77070be438b9438025b9b6d5ab35
Hasan Ay <hasanay777@gmail.com>
2021-11-14 12:31:48 +0300

    Prc3

---
+++ src/test/java/com/mentor/practice/Prc2.java
@@ -0,0 +1,7 @@
+package com.mentor.practice;
+
+public class Prc2 {
+    public static void main(String[] args) {
+        // lo lo yaptık
+    }
+}
---
+++ src/test/java/com/mentor/practice/Prc3.java
@@ -0,0 +1,8 @@
+package com.mentor.practice;
+
+public class Prc3 {
+    public static void main(String[] args) {
+        String variable;
+
+    }
+}
